307832b36462b20588fe570b85bbcd2c5426d4f0
rmlamarche <rmlamarche@wpi.edu>
2021-02-26 11:42:29 -0500

    vega lite - this is kinda cool!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(7777);
+app.listen(7777, () => {
+	console.log('http://localhost:7777/');
+});
 
 app.use(express.static(__dirname));
